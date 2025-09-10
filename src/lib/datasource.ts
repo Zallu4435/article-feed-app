94a1f8861d29b95d6fd55990f449195d958ba105
Muhammed Nazal <muhammednazal28@gmail.com>
2025-09-10 13:15:34 +0530

    DataSounce updated to fix bug

--- src/lib/datasource.ts
+++ src/lib/datasource.ts
@@ -10,11 +10,8 @@ export const AppDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-  synchronize: process.env.NODE_ENV === "development",
-  entities:
-    process.env.NODE_ENV === "production"
-      ? ["dist/entities/*.js"]
-      : [User, Article, Category, UserPreference, RefreshToken, EmailVerification],
+  synchronize: process.env.DB_SYNC === "true" || process.env.NODE_ENV === "development",
+  entities: [User, Article, Category, UserPreference, RefreshToken, EmailVerification],
   migrations: [],
   subscribers: [],
   extra: {
